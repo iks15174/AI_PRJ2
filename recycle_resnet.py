ebf0ede6cfa8ae1f8a7b7c3f83624b031f306a79
ParkJiHo <iks15174@daum.net>
2022-05-22 14:54:24 +0900

    fix: update resnet model

--- recycle_resnet.py
+++ recycle_resnet.py
@@ -58,7 +58,7 @@ class BasicBlock(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_block, num_classes=10, init_weights=True):
+    def __init__(self, block, num_block, num_classes=6):
         super().__init__()
 
         # our image size:
@@ -67,24 +67,20 @@ class ResNet(nn.Module):
 
         self.in_channels = 64
 
-        self.conv1 = nn.Sequential(
+        self.layer1 = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
         )
 
-        self.conv2_x = self._make_layer(block, 64, num_block[0], 1)
-        self.conv3_x = self._make_layer(block, 128, num_block[1], 2)
-        self.conv4_x = self._make_layer(block, 256, num_block[2], 2)
-        self.conv5_x = self._make_layer(block, 512, num_block[3], 2)
+        self.layer2 = self._make_layer(block, 64, num_block[0], 1)
+        self.layer3 = self._make_layer(block, 128, num_block[1], 2)
+        self.layer4 = self._make_layer(block, 256, num_block[2], 2)
+        self.layer5 = self._make_layer(block, 512, num_block[3], 2)
 
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
-
-        # weights inittialization
-        if init_weights:
-            self._initialize_weights()
 
     def _make_layer(self, block, out_channels, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -96,11 +92,11 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        output = self.conv1(x)
-        output = self.conv2_x(output)
-        x = self.conv3_x(output)
-        x = self.conv4_x(x)
-        x = self.conv5_x(x)
+        output = self.layer1(x)
+        output = self.layer2(output)
+        x = self.layer3(output)
+        x = self.layer4(x)
+        x = self.layer5(x)
         x = self.avg_pool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
